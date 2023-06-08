1c9314b4e5f92441bf5b1830ccef896598a5d44a
Sofia Di Blasi <phia.di.blasi@gmail.com>
2023-06-08 15:48:45 +0100

    svolgimento esercizio

---
+++ hotel.php
@@ -0,0 +1,87 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <title>Lista Hotel</title>
+</head>
+<body>
+    <table class="table">
+        <thead class="text-center">
+            <tr>
+                <th scope="col">Nome Hotel</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Parcheggio</th>
+                <th scope="col">Voto</th>
+                <th scope="col">Distanza dal centro</th>
+            </tr>
+        </thead>
+        <tbody class="text-center">
+            <?php foreach ($hotels as $hotel) { ?>
+                <tr>
+                    <td><?php echo $hotel["name"] ?></td>
+                    <td><?php echo $hotel["description"] ?></td>
+                    <!-- con un if vado a controllare il valore di parking, 
+                    faccio in modo che in base se sia vero o falso stampa un carattere visivamente più gradevole -->
+                    <td><?php 
+                        if($hotel["parking"] == true){
+                            echo "&#10003;";
+                        } else {
+                            echo  "&#10007;";
+                        }
+                    ?></td>
+                    <td><?php echo $hotel["vote"] ?></td>
+                    <td><?php echo $hotel["distance_to_center"] ?></td>
+                </tr>
+            <?php } ?> 
+        </tbody>
+    </table>
+</body>
+</html>
